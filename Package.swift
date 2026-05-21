e900492ee4db9986a4616e7554065aa9cc983a8d
parveenai <parveenk@appinfoinc.com>
2026-05-21 17:35:19 +0530

    update

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
         .binaryTarget(
             name: "TravelSafelyBoneshell",
             url: "https://github.com/parveenai/SPM-SDK/releases/download/1.0.6/TravelSafelyBoneshell.xcframework.zip",
-            checksum: "f7b497fd61ce79f3ee7b6b4a2847aa689f0bccfd6e4256216e5c87c018d2a15a"
+            checksum: "759654e268a132ba31ae6a778d27518cb802a8f67b2417244855caa465449d5a"
         )
     ]
 )
